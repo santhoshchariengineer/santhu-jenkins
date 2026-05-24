8c12ea462e1dfa10270854c6d2d8475dbbbf15a6
santhoshchariengineer <santhoshchariengineer@gmail.com>
2026-05-24 09:26:25 +0530

    Implement Jenkins pipeline for code deployment
    Add pipeline stages for fetching, validating, and deploying code.

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,1 +1,30 @@
-
+pipeline{
+  agent: any
+  stages{
+    stage('fetch code from SCM'){
+      steps{
+        echo'successfully fetched file from SCM'
+      }
+    }
+    
+    stage('validate file content from SCM'){
+      steps{
+        if(fileExists(index.html){
+          sh 'grep -q '</html>' index.html'
+          echo'validation is successfull'
+        } else {
+          error'fatal error'
+        }
+           }
+           }
+           }
+           
+    stage('Deploy code to nginx server'){
+      steps{
+        sh 'cp index.html /usr/share/nginx/html/'
+        sh 'cp image0.jpg /usr/share/nginx/html/'
+        echo'deployment success'
+      }
+    }
+  }
+}
